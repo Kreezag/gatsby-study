566c29d6d240d264d481fd490fc8b68d472034cf
Kuchuk Andrey <kreezag@gmail.com>
2021-10-17 12:26:10 +0400

    install styled components

---
+++ src/themes/theme.js
@@ -0,0 +1,29 @@
+export default {
+  fonts: {
+    main: "Multi, sans-serif",
+    code: "Roboto Mono, monospace",
+  },
+  colors: {
+    main: "hsl(207, 70%, 59%)",
+    dark: "hsl(227, 2%, 12%)",
+    light: "hsl(0, 0%, 97%)",
+  },
+  breakpoints: {
+    mobile: "only screen and (max-width: 50rem)",
+    tablet: "only screen and (max-width: 65rem)",
+  },
+  spacings: {
+    xSmall: "0.5rem",
+    small: "1rem",
+    medium: "2rem",
+    large: "3rem",
+    xLarge: "4rem",
+  },
+  animations: {
+    button: "box-shadow 0.25s easy-in-out",
+    ink: "color 0.25s easy",
+  },
+  shadows: {
+    default: "0 5px 25px rgba(30, 30, 31, 0.05)",
+  },
+}
