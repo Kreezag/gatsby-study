12515d9ee23ce146061c54083fc9f630853c79cf
Kuchuk Andrey <kreezag@gmail.com>
2021-10-19 23:50:35 +0400

    create content component

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,8 @@ import { Container } from './container'
 import { Nav } from './nav'
 import { Footer } from './footer'
 import { CoverImage } from './coverImage'
+import { Content } from './content'
 
 
 
-export { Container, Nav, Footer, CoverImage };
+export { Container, Nav, Footer, CoverImage, Content };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from "react"
 import "react-dom"
-import { Container, Nav, Footer, CoverImage } from '../components';
+import { Container, Nav, Footer, CoverImage, Content } from '../components';
 
 const IndexPage = () => {
   return (
     <Container>
       <Nav />
       <CoverImage />
+      <Content>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet autem beatae delectus eius facere ipsam necessitatibus odio quisquam quod ratione? Corporis dolorum exercitationem facilis inventore ipsum iure laboriosam mollitia quam?
+      </Content>
       
       <Footer />
     </Container>
