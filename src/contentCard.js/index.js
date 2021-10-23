da357aa8e1b01bfb969f3e3b84abadbf3f6c5243
Kuchuk Andrey <kreezag@gmail.com>
2021-10-23 11:43:41 +0400

    remove unnecessary folder

--- src/contentCard.js/index.js
+++
@@ -1,9 +0,0 @@
-import { Container } from './container'
-import { Nav } from './nav'
-import { Footer } from './footer'
-import { CoverImage } from './coverImage'
-import { Content } from './content'
-
-
-
-export { Container, Nav, Footer, CoverImage, Content };
