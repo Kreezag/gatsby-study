230e9c0a5c43b7c4e7f008f03922ea52f6cc40a7
Kuchuk Andrey <kreezag@gmail.com>
2021-10-23 14:47:19 +0400

    try to create generated all posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,48 +1,50 @@
-// export const createPage = async function ({ actions, graphql }) {
-//   const { data } = await graphql`
-//     query {
-//       allMdx(sort: { fields: frontmatter___date, order: DESC }) {
-//         edges {
-//           node {
-//             frontmatter {
-//               slug
-//             }
-//             id
-//           }
-//         }
-//       }
-//     }
-//   `
-//
-//   // Create pagination
-//
-//   const postPerPage = 3
-//
-//   const numPages = Math.ceil(data.allMdx.edges.length / postPerPage)
-//
-//   new Array(numPages).forEach((_, index) => {
-//     actions.createPage({
-//       path: index === 0 ? "/" : `/${index + 1}`,
-//       component: require.resolve("./src/templates/allPosts.js"),
-//       context: {
-//         limit: postPerPage,
-//         skip: index * postPerPage,
-//         numPages,
-//         currentPage: index + 1,
-//       },
-//     })
-//   })
-//
-//   // Create single post
-//
-//   data.allMdx.edges.forEach(edge => {
-//     const slug = edge.node.frontmatter.slug
-//     const id = edge.node.id
-//
-//     actions.createPage({
-//       path: slug,
-//       component: require.resolve("./src/templates/singlePost.js"),
-//       context: { id },
-//     })
-//   })
-// }
+const path = require(`path`)
+
+exports.createPages = async function ({ graphql, actions }) {
+  const { data } = await graphql(`
+    query {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+            id
+          }
+        }
+      }
+    }
+  `)
+  
+  const appPostsTemplate = path.resolve("./src/templates/allPosts.js")
+
+  const postPerPage = 3
+
+  const numPages = Math.ceil(data.allMdx.edges.length / postPerPage)
+
+  new Array(numPages).forEach((_, index) => {
+    actions.createPages({
+      path: index === 0 ? "/" : `/${index + 1}`,
+      component: appPostsTemplate,
+      context: {
+        limit: postPerPage,
+        skip: index * postPerPage,
+        numPages,
+        currentPage: index + 1,
+      },
+    })
+  })
+
+  // Create single post
+
+  // data.allMdx.edges.forEach(edge => {
+  //   const slug = edge.node.frontmatter.slug
+  //   const id = edge.node.id
+  //
+  //   actions.createPage({
+  //     path: slug,
+  //     component: require.resolve("./src/templates/singlePost.js"),
+  //     context: { id },
+  //   })
+  // })
+}
---
+++ src/templates/allPosts.js
@@ -0,0 +1,80 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import "react-dom"
+import { Container } from "../components/container"
+import { Nav } from "../components/nav"
+import { Footer } from "../components/footer"
+import { CoverImage } from "../components/coverImage"
+import { Content } from "../components/content"
+import { ContentCard } from "../components/contentCard"
+import { Pagination } from "../components/pagination"
+import { H1, P } from "../components/typography"
+
+
+const allPosts = ({ context, data }) => {
+  const { currentPage, numPages } = context
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = currentPage - 1 === 1 ? "/" : `/${currentPage - 1}`
+  const nextPage = `/${currentPage + 1}`
+
+  const posts = data.allMdx.edges
+
+  return (
+    <Container>
+      <Nav />
+      <CoverImage />
+      <Content>
+        <H1 textAlign="center" margin="0 0 1rem 0">
+          All posts
+        </H1>
+        <P color="dark2" textAlign="center">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
+          dignissimos dolor doloribus eos esse est illo molestiae nam, neque
+          officiis, provident quaerat recusandae sapiente sed sequi tenetur ut,
+          velit vero!
+        </P>
+        {posts.map(post => (
+          <ContentCard
+            key={post.node.frontmatter.slug}
+            date={post.node.frontmatter.date}
+            title={post.node.frontmatter.title}
+            excerpt={post.node.frontmatter.excerpt}
+            slug={post.node.frontmatter.slug}
+          />
+        ))}
+      </Content>
+      <Pagination
+        isFirst={isFirst}
+        isLast={isLast}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      />
+      <Footer />
+    </Container>
+  )
+}
+
+export const pageQuery = graphql`
+    query allPostsQuery ($skip: Int!, $limit: Int!) {
+        allMdx(
+            sort: { fields: frontmatter___date, order: DESC }
+            skip: $skip
+            limit: $limit
+        ) {
+            edges {
+                node {
+                    frontmatter {
+                        date
+                        excerpt
+                        slug
+                        title
+                    }
+                }
+            }
+        }
+    }
+`
+
+export default allPosts;
