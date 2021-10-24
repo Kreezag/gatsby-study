40c5b4ab443b3228e8d5078e75dde24d214c32e0
Kuchuk Andrey <kreezag@gmail.com>
2021-10-24 12:32:19 +0400

    add md styles

--- src/components/post.js
+++ src/components/post.js
@@ -23,6 +23,65 @@ const PostWrapper = styled.main`
     padding-left: ${(props) => props.theme.spacings.large};
     padding-right: ${(props) => props.theme.spacings.large};
   }
+  
+  h1,h2,h3,h4,h5,h6 {
+    color: ${(props) => props.theme.colors.dark1};
+  }
+
+  h1:not(:first-child),h2,h3,h4,h5,h6 {
+    margin-top: 2rem;
+  }
+  
+  h1 {
+    font-size: 2.25rem;
+    line-height: 2.5rem;
+    font-weight: 400;
+  }
+  
+  h2 {
+    font-size: 1.5rem;
+    line-height: 1.875rem;
+    font-weight: 400;
+  }
+  
+  h3 {
+    font-size: 1.375rem;
+    line-height: 1.625rem;
+    font-weight: 700;
+  }
+  
+  h4 {
+    font-size: 1.25rem;
+    line-height: 1.5rem;
+    font-weight: 400;
+  }
+  
+  h5 {
+    font-size: 1.125rem;
+    line-height: 1.375rem;
+    font-weight: 700;
+  }
+  
+  h6 {
+    font-size: 1rem;
+    line-height: 1.125rem;
+    font-weight: 700;
+    text-transform: uppercase;
+  }
+  
+  ul,
+  ol {
+    color: ${(props) => props.theme.colors.dark1};
+    margin: 1rem 0 1rem 2rem;
+  }
+  
+  li {
+    margin: 0.25rem 0;
+  }
+  
+  img {
+    max-width: 33.33%
+  }
 `
 
 export const Post = ({ children }) => (
